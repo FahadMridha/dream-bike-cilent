36f53d949272c4a590f0cc92af277ab46eb1c53a
fahad Mridha <sohelcse16@gmail.com>
2023-01-15 02:05:57 +0600

    login page modifay

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
 
   const location = useLocation();
   const navigate = useNavigate();
-  const from = location.state?.from?.pathname || "/";
+  const from = location.state?.from?.pathname || "/home";
 
   if (token) {
     navigate(from, { replace: true });
@@ -89,75 +89,87 @@ const Login = () => {
   };
 
   return (
-    <div className="h-[800px] flex justify-center items-center ">
-      <div className=" w-96 p-8 bg-green-500 rounded-lg">
-        <h3 className="text-xl text-center">Login</h3>
-        <form onSubmit={handleSubmit(handleLogin)}>
-          <div className="form-control w-full max-w-xs">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
+    <div className="bg-teal-900">
+      <div className="h-[800px] flex justify-center items-center ">
+        <div className=" w-96 p-8 text-white rounded-lg">
+          <h3 className="text-3xl ">Welcome to Dream Bike </h3>
+          <h3 className="text-xl   mb-5">Please login to continue </h3>
 
-            <input
-              {...register("email", {
-                required: "Email Address is required",
-              })}
-              placeholder="email"
-              type="email"
-              className="input input-bordered w-full max-w-xs"
-            />
-            {errors.email && (
-              <p className="text-red-600">{errors.email?.message}</p>
-            )}
-          </div>
-          <div className="form-control w-full max-w-xs">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <div className="form-control w-full mb-4 max-w-xs">
+              <input
+                {...register("email", {
+                  required: "Email Address is required",
+                })}
+                placeholder="Email address"
+                type="email"
+                className="input bg-teal-900 border-gray-500 w-full max-w-xs"
+              />
+              {errors.email && (
+                <p className="text-red-600">{errors.email?.message}</p>
+              )}
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <input
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "password should be at least 6 carecter must",
+                  },
+                })}
+                placeholder="Password"
+                type="password"
+                className="input bg-teal-900 border-gray-500 w-full max-w-xs"
+              />
+              {errors.password && (
+                <p className="text-red-600">{errors.password?.message}</p>
+              )}
+              <label className="label">
+                <span className=""> Forgate password?</span>
+              </label>
+            </div>
 
-            <input
-              {...register("password", {
-                required: "Password is required",
-                minLength: {
-                  value: 6,
-                  message: "password should be at least 6 carecter must",
-                },
-              })}
-              placeholder="password"
-              type="password"
-              className="input input-bordered w-full max-w-xs"
-            />
-            {errors.password && (
-              <p className="text-red-600">{errors.password?.message}</p>
-            )}
-            <label className="label">
-              <span className="label-text"> Forgate password?</span>
-            </label>
+            <button
+              className="btn bg-emerald-400 hover:bg-emerald-500 w-full"
+              value="Login"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="animate-pulse">
+                  <SmallSpinner />
+                </span>
+              ) : (
+                "Login"
+              )}
+            </button>
+          </form>
+          <div>
+            {loginError && <p className="text-red-600">{loginError}</p>}
           </div>
+          <p>
+            No account ?
+            <Link to="/signup" className="ml-1 text-emerald-400">
+              Sign up now
+            </Link>
+          </p>
+          <div className="divider">OR</div>
 
           <button
-            className="btn bg-green-600 hover:bg-green-700 w-full"
-            value="Login"
-            type="submit"
+            onClick={() => handlerGoogleSignIn(googleProvider)}
+            className="btn bg-emerald-400 hover:bg-emerald-500 w-full"
+            disabled={loading}
           >
-            {loading ? <SmallSpinner /> : "Sign in"}
+            {loading ? (
+              <span className="animate-pulse">
+                <SmallSpinner />
+              </span>
+            ) : (
+              "CONTINUE WITH GOOGLE"
+            )}
           </button>
-        </form>
-        <div>{loginError && <p className="text-red-600">{loginError}</p>}</div>
-        <p>
-          New to Dream Bike ?
-          <Link to="/signup" className="text-primary">
-            Create new account
-          </Link>
-        </p>
-        <div className="divider">OR</div>
-
-        <button
-          onClick={() => handlerGoogleSignIn(googleProvider)}
-          className="btn bg-green-600 hover:bg-green-700 w-full"
-        >
-          CONTINUE WITH GOOGLE
-        </button>
+        </div>
       </div>
     </div>
   );
